01d01e8928331df46706832e0fcd025c90526f13
Patroclo <gabrielly_antonio@estudante.sesisenai.org.br>
2025-04-16 16:50:16 -0300

    Add files via upload

---
+++ aulahojeeeeee/script.js
@@ -0,0 +1,73 @@
+
+//3
+
+let temDinheiro = true;
+let estaComFome = true;
+let hojeEhSexta = true;
+
+if(temDinheiro > true && estaComFome >true || hojeEhSexta > true)
+{
+    console.log("Quero pizza, meu");
+}
+else{
+    console.log("Não é sexta, tristeza");
+}
+
+//4 
+
+let fruta = "laranja,uva,morango";
+
+if(fruta === "laranja,uva,morango")
+{
+    console.log("Vou fazer seu suquinho");
+}
+if( fruta === "uva")
+{
+    console.log("Suco de uva");
+}
+if (fruta === "morango")
+{
+    console.log("fazendo o suco de morango!");
+}
+else{
+    console.log("Não vou fazer nada, choraaa");
+}
+
+
+//5
+
+let suspeito1 = "42";
+let suspeito2 = 42;
+
+if(suspeito1 == suspeito2)   
+{
+    console.log("Eles dizem a mesma coisa, mas de formas diferentes.");
+}
+if(suspeito1 === suspeito2)
+{
+    console.log("Eles são exatamente iguais.");
+}
+else 
+{ 
+    console.log("Eles são diferentes.");
+}
+
+
+//6
+
+let quantidadeItens = 3;
+let valorTotal = 100;
+
+if(valorTotal >= 100  && quantidadeDeItens >=3)
+{
+    console.log("Desconto aplicado");
+}
+else{ 
+    console.log("Sem desconto, mano");
+}
+
+
+//7
+
+let pedido = "cafe, cappucino, expresso";
+
